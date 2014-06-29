f81c2a1bfbf234c384a92f0603af490fc9fb46b3
Charles Macanka <cmacanka@gmail.com>
2014-06-29 19:25:41 -0400

    Highlight text in text boxes with keyboard focus.

--- Phlebotomist/App.xaml.cs
+++ Phlebotomist/App.xaml.cs
@@ -13,5 +13,21 @@ namespace Phlebotomist
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Select the text in a TextBox when it receives focus.
+            EventManager.RegisterClassHandler(typeof(System.Windows.Controls.TextBox), System.Windows.Controls.TextBox.GotKeyboardFocusEvent,
+                new RoutedEventHandler(SelectAllText));
+            EventManager.RegisterClassHandler(typeof(System.Windows.Controls.TextBox), System.Windows.Controls.TextBox.MouseDoubleClickEvent,
+                new RoutedEventHandler(SelectAllText));
+            base.OnStartup(e);
+        }
+
+        void SelectAllText(object sender, RoutedEventArgs e)
+        {
+            var textBox = e.OriginalSource as System.Windows.Controls.TextBox;
+            if (textBox != null)
+                textBox.SelectAll();
+        }
     }
 }
